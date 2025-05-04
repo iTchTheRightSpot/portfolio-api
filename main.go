6d848e91ab08386e9b0c8912525b0f914bad0298
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-05-04 06:09:51 -0400

    upgrade dependencies & error using cloudflare env api

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"cmp"
 	"context"
 	"github.com/iTchTheRightSpot/utility/middleware"
 	"github.com/iTchTheRightSpot/utility/utils"
 	"github.com/rs/cors"
-	"github.com/syumai/workers/cloudflare"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/syumai/workers"
 )
 
 func main() {
-	//ui := cmp.Or(os.Getenv("FRONTEND"), "http://localhost:4200")
-	//discord := cmp.Or(os.Getenv("DISCORD"), "private-channel-webhook")
-	ui := cloudflare.Getenv("FRONTEND")
-	lg := utils.ProdLogger(time.RFC3339, "UTC", cloudflare.Getenv("DISCORD"))
+	ui := cmp.Or(os.Getenv("FRONTEND"), "http://localhost:4200")
+	discord := cmp.Or(os.Getenv("DISCORD"), "private-channel-webhook")
+	//ui := cloudflare.Getenv("FRONTEND")
+	//discord := cloudflare.Getenv("DISCORD")
 
+	lg := utils.ProdLogger(time.RFC3339, "UTC", discord)
 	m := middleware.Middleware{Logger: lg}
 
 	han := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +40,6 @@ func main() {
 		AllowCredentials: true,
 	})
 
-	lg.Log(context.Background(), "server listening on default port 9900")
+	lg.Log(context.Background(), "server listening on default port 9900 "+ui)
 	workers.Serve(m.Log(c.Handler(m.Panic(han))))
 }
