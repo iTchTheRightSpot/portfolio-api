9f011a5ef31158ad569d182686148bf6ed1e544e
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-05-04 03:56:27 -0400

    local init

--- go.mod
+++ go.mod
@@ -1,5 +1,22 @@
 module github.com/syumai/workers/_templates/cloudflare/deploy-go
 
-go 1.23.0
+go 1.24.2
 
-require github.com/syumai/workers v0.29.0
+require (
+	github.com/iTchTheRightSpot/utility v0.0.0-20250420234747-763dc2c926fb
+	github.com/rs/cors v1.11.1
+	github.com/syumai/workers v0.29.0
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.26.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"cmp"
+	"context"
+	"github.com/iTchTheRightSpot/utility/middleware"
+	"github.com/iTchTheRightSpot/utility/utils"
+	"github.com/rs/cors"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/syumai/workers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		msg := "Hello, Go!"
-		w.Write([]byte(msg))
+	ui := cmp.Or(os.Getenv("FRONTEND"), "http://localhost:4200")
+	discord := cmp.Or(os.Getenv("DISCORD"), "private-channel-webhook")
+
+	lg := utils.DevLogger("UTC")
+	if discord != "private-channel-webhook" {
+		if strings.Contains(ui, "localhost") {
+			lg.Fatal("please set frontend url")
+		}
+		lg = utils.ProdLogger(time.RFC3339, "UTC", discord)
+	}
+
+	m := middleware.Middleware{Logger: lg}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /api/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := r.PathValue("name")
+		if name == "" {
+			utils.ErrorResponse(w, &utils.BadRequestError{})
+			return
+		}
+		lg.Log(r.Context(), name+" is visiting your portfolio")
+		w.WriteHeader(204)
 	})
-	workers.Serve(nil) // use http.DefaultServeMux
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{ui},
+		AllowedMethods:   []string{http.MethodPost},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Accept"},
+		ExposedHeaders:   []string{"Content-Length"},
+		AllowCredentials: true,
+	})
+
+	lg.Log(context.Background(), "server listening on default port 9900")
+	workers.Serve(m.Log(c.Handler(m.Panic(mux))))
 }
